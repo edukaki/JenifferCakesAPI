f5c12cbeb010c39a7b2dd8b54176fa7378bbf45b
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-22 21:22:34 +0900

    feat: connect with db

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import Express from 'express';
+import db from './config/dbConnect';
+
+db.on('error', console.log.bind(console, 'Connection error'));
+db.once('open', () => {
+    console.log('Connection successfull');
+});
+
+const app = Express();
+app.use(Express.json());
+
+export default app;

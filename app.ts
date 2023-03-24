52c1bf6900b0f78ad44d9f9592de12e3a84abdd6
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-24 22:09:19 +0900

    feat(app): import routes

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import Express from 'express';
 import db from './src/config/dbConnect';
+import routes from './src/routes';
 
 db.on('error', console.log.bind(console, 'Connection error'));
 db.once('open', () => {
@@ -8,5 +9,6 @@ db.once('open', () => {
 
 const app = Express();
 app.use(Express.json());
+routes(app);
 
 export default app;

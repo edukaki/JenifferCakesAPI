e4aa6a4362056bca5a919c61a30d0b5e1e53d8b0
Eduardo <eduardo.arakaki@hotmail.com>
2023-03-26 16:08:48 +0900

    feat(app): create flavors routes into routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import Express, { IRouter } from 'express';
+import flavors from './flavorsRoutes';
 import cakes from './cakesRoutes';
 
 const routes = (app: IRouter) => {
     app.route('/').get((req, res) => {
         res.status(200).send({ title: 'JenifferCakesAPI' });
-        app.use(Express.json(), cakes);
+        app.use(Express.json(), cakes, flavors);
     });
 };
 
